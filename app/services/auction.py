0be75bec7704f931af5e00c82145ef95a8265dd5
sleeter <lfmonsterfeed@gmail.com>
2024-11-10 02:14:54 +0300

    add criterion

--- app/services/auction.py
+++ app/services/auction.py
@@ -1,6 +1,6 @@
 from fastapi import Depends, HTTPException
 from app.repositories.qs import QSRepository
-from app.models.dtos import AuctionSchema, QuoteSession
+from app.models.dtos import AuctionSchema, QuoteSession, Criterion
 from http import HTTPStatus
 from .req import RequestService
 
@@ -23,6 +23,34 @@ class AuctionService:
         url = "https://zakupki.mos.ru/newapi/api/Auction/Get?auctionId=" + str(id)
 
         auction = await self.req_service.req_to_get_auction(url)
+
+        for val in Criterion:
+            match val:
+                case "name": # Проверить имя на соответствие
+                    break
+                case "executor":
+                    if auction.isContractGuaranteeRequired: # Требуется обеспечение исполнения контракта
+                        pass
+                    break
+                case "license":
+                    if len(auction.licenseFiles) > 0: # Требуется проверить наличие сертификатов/лицензий
+                        pass
+                    break
+                case "delivery_schedule":
+                    if len(auction.deliveries) > 0: # Требуется проверить на соответствие даты, место и товары
+                        pass
+                    break
+                case "max_cost":
+                    if auction.contractCost is not None: # Требуется проверить максимальную цену контракта
+                        pass
+                    break
+                case "start_cost":
+                    if auction.startCost is not None: # Должно быть значение "Цена контракта"
+                        pass
+                    break
+                case "task_document": # Проверить auction.deliveries.items на соответствие
+                    break
+
         # TODO: send auction to RabitMQ
 
         id = auction.id
