da0cee7ee2febe01065fa822b544ff26003506f0
sleeter <lfmonsterfeed@gmail.com>
2024-11-09 16:35:37 +0300

    add right behaviour to get url

--- app/services/auction.py
+++ app/services/auction.py
@@ -4,6 +4,7 @@ from app.models.dtos import AuctionUrl, QuoteSession
 from http import HTTPStatus
 from .req import RequestService
 
+
 class AuctionService:
     def __init__(
             self,
@@ -14,16 +15,21 @@ class AuctionService:
         self.qs_repository = qs_repository
         self.req_service = req_service
 
-    async def create_qs(self, auction_url: AuctionUrl):
-        auction = await self.req_service.req_to_get_auction(auction_url)
+    async def create_qs(self, auction_url: AuctionUrl) -> QuoteSession:
+        id = auction_url.url.rstrip("/").split("/")[-1]
+        if not id.isdigit():
+            raise HTTPException(HTTPStatus.UNPROCESSABLE_ENTITY)
+
+        url = "https://zakupki.mos.ru/newapi/api/Auction/Get?auctionId=" + str(id)
+
+        auction = await self.req_service.req_to_get_auction(url)
         # TODO: send auction to RabitMQ
 
         id = auction.id
-        return self.qs_repository.create_qs(id)
+        return await self.qs_repository.create_or_update_qs(id)
 
     async def get_qs(self, id: int) -> QuoteSession:
-        qs = self.qs_repository.get_qs(id)
-        if qs == None:
+        qs = await self.qs_repository.get_qs(id)
+        if qs is None:
             raise HTTPException(HTTPStatus.BAD_REQUEST)
         return qs
-
